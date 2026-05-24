1ee7cd667b562e1593b0f8a034ae32a0a18ac086
Vinicius <timelord1025@gmail.com>
2026-05-24 14:26:28 -0300

    feat: input selector

--- lib/components/pages/main_page.dart
+++ lib/components/pages/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:record/record.dart';
 import 'package:soniox_transcriptor/components/api_key_setter.dart';
 import 'package:soniox_transcriptor/repositories/hotkey_listener.dart';
 import 'package:soniox_transcriptor/repositories/recorder_repository.dart';
@@ -17,9 +18,18 @@ class _MainPageState extends State<MainPage> {
   final recorder = RecorderRepository();
   SonioxWebsocket? soniox;
 
+  List<InputDevice> _devices = [];
+  InputDevice? _selectedDevice;
+
   @override
   void initState() {
     hotkeyListener.start();
+    recorder.listInputDevices().then((_) {
+      setState(() {
+        _devices = recorder.devices;
+        _selectedDevice = recorder.selectedDevice;
+      });
+    });
     super.initState();
   }
 
@@ -47,11 +57,61 @@ class _MainPageState extends State<MainPage> {
                   _updateSonioxInstance(apiKey: value ?? '');
                 },
               ),
+              _buildDeviceSelector(context),
             ],
           ),
         ),
       ),
     );
+  }
+
+  // MARK: Widgets
+
+  Widget _buildDeviceSelector(BuildContext context) {
+    final label = _selectedDevice?.label ?? 'Default';
+    return Row(
+      children: [
+        const Text('Input device'),
+        const Spacer(),
+        CupertinoButton(
+          padding: EdgeInsets.zero,
+          onPressed: _devices.isEmpty ? null : () => _showDevicePicker(context),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(label),
+              const SizedBox(width: 4),
+              const Icon(CupertinoIcons.chevron_up_chevron_down, size: 14),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
+  void _showDevicePicker(BuildContext context) {
+    final initialIndex = _selectedDevice == null
+        ? 0
+        : _devices.indexWhere((d) => d.id == _selectedDevice!.id).clamp(0, _devices.length - 1);
+    var pickerIndex = initialIndex;
+
+    showCupertinoModalPopup<void>(
+      context: context,
+      builder: (_) => Container(
+        height: 216,
+        color: CupertinoColors.systemBackground.resolveFrom(context),
+        child: CupertinoPicker(
+          scrollController: FixedExtentScrollController(initialItem: initialIndex),
+          itemExtent: 36,
+          onSelectedItemChanged: (i) => pickerIndex = i,
+          children: _devices.map((d) => Center(child: Text(d.label))).toList(),
+        ),
+      ),
+    ).then((_) {
+      final picked = _devices[pickerIndex];
+      setState(() => _selectedDevice = picked);
+      recorder.selectedDevice = picked;
+    });
   }
 
   // MARK: Events
